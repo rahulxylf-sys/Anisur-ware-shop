6b4648f1d53e16378e2ccda77c6cafdcf884c86a
VYNZO GAME <rahulxylf@gmail.com>
2026-09-23 13:04:57 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+export const firebaseConfig = {
+
+    apiKey: "PASTE_API_KEY",
+
+    authDomain:
+    "PASTE_PROJECT_ID.firebaseapp.com",
+
+    projectId:
+    "PASTE_PROJECT_ID",
+
+    storageBucket:
+    "PASTE_PROJECT_ID.firebasestorage.app",
+
+    messagingSenderId:
+    "PASTE_SENDER_ID",
+
+    appId:
+    "PASTE_APP_ID"
+
+};
